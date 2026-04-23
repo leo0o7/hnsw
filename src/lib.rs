14faea252a6efec35535744e166ac0efcda9a0a9
leo0o7 <giardinoleonardo34@gmail.com>
2026-04-23 11:25:05 +0200

    hnsw init

--- src/lib.rs
+++ src/lib.rs
@@ -17,4 +17,22 @@ struct Hnsw<const D: usize> {
     nodes: Vec<Node>,
 }
 
-impl<const D: usize> Hnsw<D> {}
+impl<const D: usize> Hnsw<D> {
+    #[allow(non_snake_case)]
+    pub fn new(M: usize, M0: usize, ef_construction: usize, ef_search: usize) -> Self {
+        Self {
+            M,
+            M0,
+            ef_construction,
+            ef_search,
+            entry_point: 0,
+            data: Vec::new(),
+            nodes: Vec::new(),
+        }
+    }
+    #[allow(non_snake_case)]
+    pub fn new_default(M: usize) -> Self {
+        Self::new(M, 2 * M, 128, 32)
+    }
+}
+
